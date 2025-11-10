b7fa410889ee67a210bacfe9660739af0563145b
Louie Shen <louieshen@MacBookPro.attlocal.net>
2025-11-09 20:34:38 -0800

    rom

---
+++ final/player_rom.v
@@ -0,0 +1,27 @@
+module player(
+    input wire  clk,
+    input wire [9:0] row,             //// TOOD adjust input
+    input wire [9:0] col,
+    output reg [11:0] color_data
+);
+
+//// signal_declaration
+reg [:] row_reg;
+reg [:] col_reg;
+
+always @(posedge clk) begin
+    row_reg <= row;
+    col_reg <= col;
+end
+
+always @(*)
+    case({row_reg, col_reg})
+    
+    ////////////////
+    ///color data///
+    ////////////////
+    
+    endcase
+
+
+endmodule
